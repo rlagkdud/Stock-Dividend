01c355831fea62850518636166ee5e643b78b6a2
rlagkdud <ha0936861@gmail.com>
2023-05-23 11:29:21 +0900

    api 설계

---
+++ src/main/java/com/dayone/web/Controller.java
@@ -0,0 +1,12 @@
+package com.dayone.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller {
+
+
+
+
+}
