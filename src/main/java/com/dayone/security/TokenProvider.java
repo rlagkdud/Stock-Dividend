371a4846026dbba08d5f121ed3ab8dff2c6fed53
rlagkdud <ha0936861@gmail.com>
2023-05-26 11:48:25 +0900

    회원관리 - JWT : 토큰 생성, 토큰 유효성 확인

--- src/main/java/com/dayone/security/TokenProvider.java
+++ src/main/java/com/dayone/security/TokenProvider.java
@@ -1,4 +1,80 @@
 package com.dayone.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class TokenProvider {
+
+    private static final String KEY_ROLES = "roles";
+    private static final long TOKEN_EXPIRED_TIME = 1000 * 60 * 60; // 1 hour
+
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
+
+    /**
+     * 토큰 생성(발급)
+     * @param username
+     * @param roles
+     * @return
+     */
+    public String generateToken(String username, List<String> roles) {
+
+        // 사용자 정보
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles);
+
+        // 만료 시간
+        var now = new Date();
+        var expiredDate = new Date(now.getTime() + TOKEN_EXPIRED_TIME);
+
+        // 토큰 생성
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now) // 토큰 생성시간
+                .setExpiration(expiredDate) // 토큰 만료 시간
+                .signWith(SignatureAlgorithm.HS512, this.secretKey) // 사용할 암호화 알고리즘, 비밀키
+                .compact();
+    }
+
+    // 클래임 정보에서, username 가져오기
+    public String getUsername(String token) {
+        return this.parseClaims(token).getSubject();
+    }
+
+
+    /**
+     * 토큰 유효성 검사
+     * @param token
+     * @return
+     */
+    public boolean validateToken(String token) {
+        // 토큰 값이 없다면 false
+        if (!StringUtils.hasText(token)) return false;
+
+        // 만료 된 토큰이면 false
+        var claims = this.parseClaims(token);
+        return !claims.getExpiration().before(new Date());
+    }
+
+    // 토큰 파싱(클래임 정보 가져오기)
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(this.secretKey).parseClaimsJwt(token).getBody();
+        } catch (ExpiredJwtException e) {
+            // TODO
+            return e.getClaims();
+        }
+    }
 }
