0d8dd7e00864f3c581e1442a4add2a0f6f5feb59
rlagkdud <ha0936861@gmail.com>
2023-05-29 14:30:55 +0900

    로그 남기기

--- src/main/java/com/dayone/scheduler/ScraperScheduler.java
+++ src/main/java/com/dayone/scheduler/ScraperScheduler.java
@@ -50,6 +50,7 @@ public class ScraperScheduler {
                         boolean exists = dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
                         if(!exists){
                             this.dividendRepository.save(e);
+                            log.info("insert new dividend -> " + e.toString());
                         }
                     });
             // 연속적으로 스크래핑 대상 사이트 서버에 요청을 날리지 않도록 일시정지
--- src/main/java/com/dayone/web/AuthController.java
+++ src/main/java/com/dayone/web/AuthController.java
@@ -33,6 +33,7 @@ public class AuthController {
         // 토큰을 생성해준다.
         var member = this.memberService.authenticate(request);
         var token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+        log.info("user login -> "+ request.getUsername());
         return ResponseEntity.ok(token);
     }
 }
